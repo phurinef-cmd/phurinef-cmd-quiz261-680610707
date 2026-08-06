bb4cf0c5b1f184082e9f11a45ea1f6b4bfd5c6f4
Ef <phurin.ef@gmail.com>
2026-08-06 11:13:52 +0700

    quiz2

--- src/routes/itemsRoutes.ts
+++ src/routes/itemsRoutes.ts
@@ -77,7 +77,7 @@ router.post("/", async (req: Request, res: Response) => {
     if (found) {
       return res.status(400).json({
         success: false,
-        message: "Student is already exists",
+        message: "item is already exists",
       });
     }
 
